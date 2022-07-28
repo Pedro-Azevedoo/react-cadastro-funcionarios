8eb3946d7220d3bd420b05e16bc5c65dc5170b06
pedroa.desenvolvimento@outlook.com <pedroa.desenvolvimento@outlook.com>
2022-07-28 17:11:15 -0300

    Alteração de Foto de Perfil Invalida

--- src/components/ColaboradorCard/index.js
+++ src/components/ColaboradorCard/index.js
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import './styles.css';
 
 const ColaboradorCard = (props) =>{
+
+    const [urlImage, setUrlImage] = useState(props.imagem);
+    const [errorImg, setErrorImg] = useState(false);
+
+    
+    function alerta(){
+        setUrlImage('https://intranet.supersoft.com.br/usuarios/fotos/anonimo.png');
+        
+    }
+
     return(
         <div className='rectangle'>
             <div className='colorSector' style={{backgroundColor: props.corPrim  }}>
                 <div className='img' style={{borderColor: props.corPrim}}>
-                    <img src={props.imagem} alt={props.nome}  />
+                    <img id="img" src={urlImage} alt={props.nome} onError={alerta} />
                 </div>
             </div>
             
